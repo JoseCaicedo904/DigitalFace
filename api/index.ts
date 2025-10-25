782782835e66de0a96599e940ee491c5b5c957b7
Jose Caicedo <jdcaicedoga@outlook.com>
2025-10-24 21:05:20 -0500

    Update Vercel Deployment

---
+++ api/index.ts
@@ -0,0 +1,13 @@
+import serverless from "serverless-http";
+
+import { createServer } from "../server";
+
+const handler = serverless(createServer());
+
+export default handler;
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
